9d0c068970247ffa07338aa2a5d9a6320317239f
Gabriel PC <gabrieljbr@gmail.com>
2024-09-15 11:30:09 -0300

    Defining signals and beahviour for turnout 102

--- myAutomation.h
+++ myAutomation.h
@@ -4,11 +4,11 @@
 // SERVO_TURNOUT(id, pin, active_angle, inactive_angle, profile [, "description"])
 
 // ----LMain-M1-Lead----
-// LMain-M1-Lead
-SIGNAL(111, 110, 109) // [S 111] Main->M1
-SIGNAL(115, 114, 113) // [S 115] Main->Lead
-SIGNAL(105, 0, 104) // [S 105] M1->Main
-SIGNAL(107, 106, 0) // [S 115] Lead->Main
+
+SIGNAL(111, 110, 109) // [S 111] LMain->M1
+SIGNAL(115, 114, 113) // [S 115] LMain->Lead
+SIGNAL(105, 0, 104) // [S 105] M1->LMain
+SIGNAL(107, 106, 0) // [S 115] Lead->LMain
 
 // [T 101 Left-Main-Lead]
 SERVO_TURNOUT(101, 101, 255, 210, Medium, "Left-Main-Lead")
@@ -17,8 +17,10 @@ SERVO_TURNOUT(101, 101, 255, 210, Medium, "Left-Main-Lead")
 
 // ----RMain-M1-M2----
 
-SIGNAL(154, 153, 152) // [S 154] Main->M1
-SIGNAL(158, 157, 156) // [S 158] Main->M2
+SIGNAL(154, 153, 152) // [S 154] RMain->M1
+SIGNAL(158, 157, 156) // [S 158] RMain->M2
+SIGNAL(161, 0, 160) // [S 161] M1->RMain
+SIGNAL(163, 162, 0) // [S 163] M2->RMain
 
 // [T 102 Right-Main-Main2]
 SERVO_TURNOUT(102, 148, 200, 370, Medium, "Right-Main-Main2")
@@ -35,6 +37,7 @@ SERVO_TURNOUT(107, 134, 200, 400, Medium, "Lead-Yard")
 AUTOSTART
 
 ONCHANGE_TURNOUT_101()
+ONCHANGE_TURNOUT_102()
 
 DONE
 
@@ -47,5 +50,17 @@ ONCLOSE(101)
 ONTHROW(101)
 
   ONCHANGE_TURNOUT_101()
+
+  DONE
+
+ONCLOSE(102)
+
+  ONCHANGE_TURNOUT_102()
+
+  DONE
+
+ONTHROW(102)
+
+  ONCHANGE_TURNOUT_102()
 
   DONE
