bc948425199792be52e6650a8fee29892584c82a
Gabriel PC <gabrieljbr@gmail.com>
2024-11-03 17:14:08 -0300

    Flashing lights

--- signalStates.h
+++ signalStates.h
@@ -1,3 +1,10 @@
+#pragma once
+
+ALIAS(M2_YARD, 153)
+ALIAS(YARD_M2)
+VIRTUAL_TURNOUT(M2_YARD,"is flashing M2 to Yard Lead")
+VIRTUAL_TURNOUT(YARD_M2,"is flashing Yard Lead to M2")
+
 // ONE HEAD
 #define CLEAR_ONE_HEAD(head) GREEN(head)
 #define APPROACH_ONE_HEAD(head) AMBER(head)
@@ -6,7 +13,7 @@
 // TWO HEADS
 #define CLEAR_TWO_HEADS(top, bottom) GREEN(top) RED(bottom)
 #define APPROACH_TWO_HEADS(top, bottom) AMBER(top) RED(bottom)
-#define APPROACH_MEDIUM_TWO_HEADS(top, bottom) AMBER(top) RED(bottom) // flashing
+#define APPROACH_MEDIUM_TWO_HEADS(top, bottom, lightVpin) AMBER_FLASHING(top, lightVpin) RED(bottom) // flashing
 #define DIVERGING_APPROACH_TWO_HEADS(top, bottom) RED(top) AMBER(bottom)
 #define DIVERGING_APPROACH_MEDIUM_TWO_HEADS(top, bottom) RED(top) AMBER(bottom) // flashing
 #define STOP_TWO_HEADS(top, bottom) RED(top) RED(bottom)
@@ -18,3 +25,12 @@
 #define STOP_THREE_HEADS(top, middle, bottom) RED(top) RED(middle) RED(bottom)
 #define DIVERGING_APPROACH_THREE_HEADS(top, middle, bottom) RED(top) AMBER(middle) RED(bottom)
 #define DIVERGING_RESTRICTED_DIVERGING_THREE_HEADS(top, middle, bottom) RED(top) RED(middle) RED(bottom) // flashing
+
+#define AMBER_FLASHING(signalVpin, lightVpin) \
+SEQUENCE(123) \
+  THROW(M2_YARD)
+  SET(lightVpin) \
+  DELAY(500) \
+  RESET(lightVpin) \
+  DELAY(500) \
+  FOLLOW(123)
